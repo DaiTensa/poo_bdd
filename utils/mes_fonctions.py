86fee8ef5de080c0deeb8c37e9c1b04c1f97e336
DaiTensa <dai.tensaout@gmail.com>
2024-05-27 16:16:34 +0200

    utils mes_fonctions

---
+++ utils/mes_fonctions.py
@@ -0,0 +1,20 @@
+import csv
+
+def import_books_from_csv(csv_file_path, session, model):
+    with open(csv_file_path, newline='', encoding='ISO-8859-1') as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=';', quotechar='"', escapechar='\\')
+        number_of_rows = 0
+
+        for row in csvreader:
+            if number_of_rows >=29:
+                break
+            book = model(
+                ISBN=row['ISBN'],
+                Book_Title=row['Book-Title'],
+                Book_Author=row['Book-Author'],
+                Year_Of_Publication= int(row['Year-Of-Publication']),
+                Publisher=row['Publisher']
+            )
+            session.add(book)
+            number_of_rows += 1
+        session.commit()

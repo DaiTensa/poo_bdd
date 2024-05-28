f77ff722826b4e881102dfcf8561bb1e8d7b81a6
DaiTensa <dai.tensaout@gmail.com>
2024-05-28 09:39:12 +0200

    réponse 5

--- src/code_to_update.py
+++ src/code_to_update.py
@@ -7,6 +7,7 @@ from models import Base
 from utils.mes_fonctions import import_books_from_csv 
 from sqlalchemy.orm import sessionmaker
 from models import Book
+from sqlalchemy import select
 
 
 
@@ -15,6 +16,7 @@ db_path = 'sqlite:///books.db'
 
 # Configuration de la base de données
 engine = create_engine(db_path)
+Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
@@ -22,5 +24,14 @@ session = Session()
 
 
 # Chemin vers le fichier CSV
+# Modification de l'affichage d'un livre avec la méthode __repr__
 csv_file_path = '/home/dai/Documents/Python_Projects/sql/sqlite_projects/data/books.csv'
 import_books_from_csv(csv_file_path, session= session, model=Book)
+
+# 5- Nombre de livres importés :
+rows = session.query(Book).count()
+print(f"Nombre de livres importés : {rows}")
+
+the_hobbit_books = select(Book.Book_Title).where(Book.Book_Title.like('%The Hobbit'))
+for title in session.scalars(the_hobbit_books):
+    print(title)
